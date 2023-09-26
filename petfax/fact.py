dd2002669ca7da7827dbeb71661259812db30421
SiaESin <siabhon.sindab@gmail.com>
2023-09-25 18:57:46 -0700

    submit.html page update

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template 
+from flask import ( Blueprint, render_template, request, redirect )
 
 bp = Blueprint('fact', __name__, url_prefix='/facts')
 
 @bp.route('/submit')
 def submit():
     return render_template('submit.html')
+
+@bp.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+        return redirect('/facts')
+    return 'This is the Facts Index!'
